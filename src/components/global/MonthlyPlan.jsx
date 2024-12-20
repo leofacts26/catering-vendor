ba303ff80d046e836d9cfa197f91aec29eb3eff5
leofacts26 <leofacts26@gmail.com>
2024-12-20 08:35:38 +0530

    Bug fixes

--- src/components/global/MonthlyPlan.jsx
+++ src/components/global/MonthlyPlan.jsx
@@ -15,7 +15,7 @@ const MonthlyPlan = () => {
     const { subscriptionData, isLoading } = useSelector((state) => state.subscription);
     const dispatch = useDispatch();
 
-    console.log(subscriptionData, "subscriptionDatasubscriptionDatasubscriptionData");
+    // console.log(subscriptionData, "subscriptionDatasubscriptionDatasubscriptionData");
 
 
     useEffect(() => {
@@ -24,6 +24,8 @@ const MonthlyPlan = () => {
 
     // onHandleSubscribe 
     const onHandleSubscribe = async (item) => {
+        // console.log(item, "item112");
+        
         await dispatch(setSubscribeData(item))
         const subscriptionDuration = "monthly";
         const newItem = {
@@ -43,57 +45,90 @@ const MonthlyPlan = () => {
 
     return (
         <>
-            <Grid container spacing={2}>
-                {subscriptionData?.length > 0 && subscriptionData?.map((item, index) => {
-                    let color = '';
-                    const subscriptionType = item?.subscriptionType?.toLowerCase();
-
-                    if (subscriptionType === 'normal') {
-                        color = 'normal-color';
-                    } else if (subscriptionType === 'popular') {
-                        color = 'popular-color';
-                    } else if (subscriptionType === 'branded') {
-                        color = 'branded-color';
-                    }
-
-                    return (
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4} className='mb-3' style={{ display: 'flex', justifyContent: 'center', padding: '0px 15px' }} key={index}>
-                            <Stack className="subscription-plans-shadow" justifyContent="space-between">
-                                <div className="sub-box-violet">
-                                    <div className={`sub-box-violet-title ${color}`}>
-                                        <h3 className="sub-box-name"> <span style={{ textTransform: 'capitalize' }}>{item?.subscriptionType}</span>  Caterer</h3>
-                                    </div>
-                                    <div className="sub-body px-2 pt-2">
-                                        <div className="sub-price">
-                                            <h3 className="text-center"> {`₹ ${item?.monthlyCharges}`} / <sub className="sub-plan-month">month</sub></h3>
+            {subscriptionData?.filter(item => item.plans?.length > 0).length > 0 ? (
+                <Grid container spacing={2}>
+                    {subscriptionData
+                        .filter(item => item.plans?.length > 0) // Filter out items with empty plans
+                        .map((item, index) => {
+                            let color = '';
+                            const subscriptionType = item?.subscriptionType?.toLowerCase();
+    
+                            if (subscriptionType === 'normal') {
+                                color = 'normal-color';
+                            } else if (subscriptionType === 'popular') {
+                                color = 'popular-color';
+                            } else if (subscriptionType === 'branded') {
+                                color = 'branded-color';
+                            }
+    
+                            return (
+                                <Grid
+                                    item
+                                    xs={12}
+                                    sm={6}
+                                    md={6}
+                                    lg={4}
+                                    xl={4}
+                                    className="mb-3"
+                                    style={{ display: 'flex', justifyContent: 'center', padding: '0px 15px' }}
+                                    key={index}
+                                >
+                                    <Stack className="subscription-plans-shadow" justifyContent="space-between">
+                                        <div className="sub-box-violet">
+                                            <div
+                                                className={`sub-box-violet-title`}
+                                                style={{ backgroundColor: `${item.subscriptionTypeDisplayColor}` }}
+                                            >
+                                                <h3 className="sub-box-name">
+                                                    <span style={{ textTransform: 'capitalize' }}>
+                                                        {item?.subscriptionType}
+                                                    </span>{' '}
+                                                    Caterer
+                                                </h3>
+                                            </div>
+                                            <div className="sub-body px-2 pt-2">
+                                                <div className="sub-price">
+                                                    <h3 className="text-center">
+                                                        {`₹ ${item?.monthlyCharges}`} /{' '}
+                                                        <sub className="sub-plan-month">month</sub>
+                                                    </h3>
+                                                </div>
+                                                <p className="sub-plan-brand mb-3 mt-3">
+                                                    List as {item?.subscriptionType} Caterer
+                                                </p>
+                                                <p className="sub-plan-para">Benefits:</p>
+                                                {item?.benefits &&
+                                                    Object.entries(item.benefits).map(([key, benefit], index) => (
+                                                        <p className="sub-plan-para" key={key}>
+                                                            - {benefit}
+                                                        </p>
+                                                    ))}
+                                                <br />
+                                            </div>
                                         </div>
-                                        <p className="sub-plan-brand mb-3 mt-3">List as {item?.subscriptionType} Caterer</p>
-                                        <p className="sub-plan-para">Benifits:</p>
-                                        {item?.benefits && Object.entries(item.benefits).map(([key, benefit], index) => (
-                                            <p className="sub-plan-para" key={key}>- {benefit}</p>
-                                        ))}
-
-
-                                        <br />
-                                    </div>
-                                </div>
-                                <div className="">
-
-
-                                    <Link to="javascript:void(0)" className="text-decoration-none mt-3">
-                                        <Button variant="contained" className={`sub-plan-btn mx-auto taxt-center ${color}`}
-                                            onClick={() => onHandleSubscribe(item)}
-                                        > Subscribe Now </Button>
-                                    </Link>
-                                    <br />
-                                </div>
-                            </Stack>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+                                        <div>
+                                            <Link to="javascript:void(0)" className="text-decoration-none mt-3">
+                                                <Button
+                                                    variant="contained"
+                                                    className={`sub-plan-btn mx-auto taxt-center ${color}`}
+                                                    onClick={() => onHandleSubscribe(item)}
+                                                >
+                                                    Subscribe Now
+                                                </Button>
+                                            </Link>
+                                            <br />
+                                        </div>
+                                    </Stack>
+                                </Grid>
+                            );
+                        })}
+                </Grid>
+            ) : (
+                <p>No subscription plans available at the moment.</p>
+            )}
         </>
-    )
+    );
+    
 }
 
 export default MonthlyPlan
--- src/components/global/YearlyPlan.jsx
+++ src/components/global/YearlyPlan.jsx
@@ -43,55 +43,90 @@ const YearlyPlan = () => {
 
     return (
         <>
-            <Grid container spacing={2}>
-                {subscriptionData?.length > 0 && subscriptionData?.map((item, index) => {
-                    let color = '';
-                    const subscriptionType = item?.subscriptionType?.toLowerCase();
-
-                    if (subscriptionType === 'normal') {
-                        color = 'normal-color';
-                    } else if (subscriptionType === 'popular') {
-                        color = 'popular-color';
-                    } else if (subscriptionType === 'branded') {
-                        color = 'branded-color';
-                    }
-                    return (
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4} className='mb-3' style={{ display: 'flex', justifyContent: 'center', padding: '0px 15px' }} key={index}>
-                            <Stack className="subscription-plans-shadow" justifyContent="space-between">
-                                <div className="sub-box-violet">
-                                    <div className={`sub-box-violet-title ${color}`}>
-                                        <h3 className="sub-box-name"> <span style={{ textTransform: 'capitalize' }}>{item?.subscriptionType}</span> Caterer</h3>
-                                    </div>
-                                    <div className="sub-body px-2 pt-2">
-                                        <div className="sub-price">
-                                            <h3 className="text-center"> {`₹ ${item?.yearlyCharges}`} / <sub className="sub-plan-month">Yearly</sub></h3>
+            {subscriptionData?.filter(item => item.plans?.length > 0).length > 0 ? (
+                <Grid container spacing={2}>
+                    {subscriptionData
+                        .filter(item => item.plans?.length > 0) // Filter subscriptions with plans
+                        .map((item, index) => {
+                            let color = '';
+                            const subscriptionType = item?.subscriptionType?.toLowerCase();
+    
+                            if (subscriptionType === 'normal') {
+                                color = 'normal-color';
+                            } else if (subscriptionType === 'popular') {
+                                color = 'popular-color';
+                            } else if (subscriptionType === 'branded') {
+                                color = 'branded-color';
+                            }
+    
+                            return (
+                                <Grid
+                                    item
+                                    xs={12}
+                                    sm={6}
+                                    md={6}
+                                    lg={4}
+                                    xl={4}
+                                    className="mb-3"
+                                    style={{ display: 'flex', justifyContent: 'center', padding: '0px 15px' }}
+                                    key={index}
+                                >
+                                    <Stack className="subscription-plans-shadow" justifyContent="space-between">
+                                        <div className="sub-box-violet">
+                                            <div
+                                                className={`sub-box-violet-title`}
+                                                style={{ backgroundColor: `${item.subscriptionTypeDisplayColor}` }}
+                                            >
+                                                <h3 className="sub-box-name">
+                                                    <span style={{ textTransform: 'capitalize' }}>
+                                                        {item?.subscriptionType}
+                                                    </span>{' '}
+                                                    Caterer
+                                                </h3>
+                                            </div>
+                                            <div className="sub-body px-2 pt-2">
+                                                <div className="sub-price">
+                                                    <h3 className="text-center">
+                                                        {`₹ ${item?.yearlyCharges}`} /{' '}
+                                                        <sub className="sub-plan-month">Yearly</sub>
+                                                    </h3>
+                                                </div>
+                                                <p className="sub-plan-brand mb-3 mt-3">
+                                                    List as {item?.subscriptionType} Caterer
+                                                </p>
+                                                <p className="sub-plan-para">Benefits:</p>
+                                                {item?.benefits &&
+                                                    Object.entries(item.benefits).map(([key, benefit], index) => (
+                                                        <p className="sub-plan-para" key={key}>
+                                                            - {benefit}
+                                                        </p>
+                                                    ))}
+                                                <br />
+                                            </div>
                                         </div>
-                                        <p className="sub-plan-brand mb-3 mt-3">List as {item?.subscriptionType} Caterer</p>
-                                        <p className="sub-plan-para">Benifits:</p>
-                                        {item?.benefits && Object.entries(item.benefits).map(([key, benefit], index) => (
-                                            <p className="sub-plan-para" key={key}>- {benefit}</p>
-                                        ))}
-
-                                        <br />
-                                    </div>
-                                </div>
-                                <div className="">
-
-
-                                    <Link to="javascript:void(0)" className="text-decoration-none mt-3">
-                                        <Button variant="contained" className={`sub-plan-btn mx-auto taxt-center ${color}`}
-                                            onClick={() => onHandleSubscribe(item)}
-                                        > Subscribe Now </Button>
-                                    </Link>
-                                    <br />
-                                </div>
-                            </Stack>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+                                        <div>
+                                            <Link to="javascript:void(0)" className="text-decoration-none mt-3">
+                                                <Button
+                                                    variant="contained"
+                                                    className={`sub-plan-btn mx-auto taxt-center ${color}`}
+                                                    onClick={() => onHandleSubscribe(item)}
+                                                >
+                                                    Subscribe Now
+                                                </Button>
+                                            </Link>
+                                            <br />
+                                        </div>
+                                    </Stack>
+                                </Grid>
+                            );
+                        })}
+                </Grid>
+            ) : (
+                <p>No yearly subscription plans available at the moment.</p>
+            )}
         </>
-    )
+    );
+    
 }
 
 export default YearlyPlan
