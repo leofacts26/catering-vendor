b2aa5562e435841b9c7b115ada75a3405fc49967
leofacts26 <leofacts26@gmail.com>
2025-02-09 22:04:45 +0530

    bug fixx

--- src/components/global/InquiryCard.jsx
+++ src/components/global/InquiryCard.jsx
@@ -2,6 +2,7 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import PhoneIcon from '@mui/icons-material/Phone';
 import toast from 'react-hot-toast';
+import moment from "moment";
 
 const InquiryCard = ({ item }) => {
 
@@ -20,9 +21,13 @@ const InquiryCard = ({ item }) => {
             <Stack className="inquiries-box mb-3" direction="row" justifyContent="space-between" alignItems="center">
                 <div>
                     <h4 className="inquiries-title">{item?.user_name}</h4>
-                    <p className="inquiries-date">{item?.enquiry_date.slice(0, 10)}</p>
+                    <p className="inquiries-date">{item?.area} </p>
+                    <p className="inquiries-date"> {item.food_types.length > 0 && item.food_types?.map((item) => item?.food_type_name)} </p>
+                    <p className="inquiries-desc mb-1">{item?.cuisines.length > 0 && item?.cuisines.map((item) => <span>{item.cuisine_name}, {" "}</span> )}</p>
+                    <p className="inquiries-desc mb-1">{item?.service_types.length > 0 && item?.service_types.map((item) => <span>{item.service_type_name}, {" "}</span> )}</p>
+                    <p className="inquiries-desc mb-1">{item?.occasions.length > 0 && item?.occasions.map((item) => <span>{item.occasion_name}, {" "}</span> )}</p>
+                    <p className="inquiries-date">  {moment(item?.enquiry_date).format("DD MMM, YYYY")}</p>
                     <p className="inquiries-desc">{item?.user_phone_number}</p>
-                    {/* <p className="inquiries-desc mt-1">{item?.description}</p> */}
                 </div>
                 <div>
                     <Button variant="contained" className="inquiries-btn-green" onClick={() => handleCopyText(item?.user_phone_number)}> <PhoneIcon style={{ fontSize: '14px' }} className="me-2" /> Call now</Button>
