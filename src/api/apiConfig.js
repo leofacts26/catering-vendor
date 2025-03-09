e62394a6d8fb3d473a05e23a73b867112f9d7548
leofacts26 <leofacts26@gmail.com>
2025-03-09 10:35:41 +0530

    token fix

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { logout } from '../features/user/userSlice';
 export const BASE_URL = 'https://api.cateringsandtiffins.in';
 
 // Create an Axios instance with common configuration options
@@ -9,3 +9,16 @@ export const api = axios.create({
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 });
+
+
+// Axios response interceptor
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            const { store } = await import('../app/store'); // Lazy import store
+            store.dispatch(logout()); // Dispatch logout action
+        }
+        return Promise.reject(error);
+    }
+);
